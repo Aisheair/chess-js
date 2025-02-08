e33ba2aa8f086ee030bba267a8453e7264e33837
Aisheair <ankitkargupta98313@gmail.com>
2025-02-08 21:40:39 +0530

    free movement of chess Pieces

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("hi");
+// console.log("hi");
+const pcs = document.querySelectorAll(".pcs")
+const box = document.querySelectorAll(".box")
+
+let selectedPiece = null;//selected chess piece
+let selectedBox = null;//original box before moving
+
+//Selecting a Piece
+pcs.forEach(p => {
+    p.addEventListener("click", selectPiece);
+})
+//Clicking a Square to Move a Piece
+box.forEach(b => {
+    b.addEventListener("click", movePiece);
+})
+
+//You Click a Piece
+function selectPiece(event){
+    if(selectedPiece) {
+        selectedPiece.classList.remove("selected")
+    }
+    selectedPiece = event.target 
+    selectedBox = selectedPiece.parentElement
+    selectedPiece.classList.add("selected"); 
+}
+// clicking on a box to move the selected pcs
+function movePiece(event){
+    if (!selectedPiece) return;
+
+    const targetBox = event.currentTarget;
+
+    if (!targetBox.querySelector(".pcs")){ // if the squar is empty
+        targetBox.appendChild(selectedPiece); // move the piece to the new box.
+        selectPiece.classList.remove("selected")
+        selectedPiece = null;
+        selectedBox = null;
+    }
+}
+
